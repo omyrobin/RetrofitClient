dc9a6365ceeab698bc1d68b0b9b982733e27b088
omyrobin <omy_robin@126.com>
2016-12-22 18:38:00 +0800

    新增上传图片地址

--- app/src/main/java/com/http/retrofitclient/constant/Url.java
+++ app/src/main/java/com/http/retrofitclient/constant/Url.java
@@ -14,8 +14,15 @@ public class Url {
 	
 	public static final String SEARCH = "api/v2/story/common/search/10/%s?%s&%sdesc=1";//搜索
 	
-	public static final String SEARCH_KEYWORD = "/api/v2/story/common/search?q=%s";//搜索关键字
-
 	public static final String READING ="m/clientview/view_chapter_content?id=%s";//根据章节ID阅读
+
+	public static final String UPLOAD_IMAGE = "http://p.duyao001.com/%supload_image_s.do";//图片上传
+
+	public static final String getUploadImageUrl(String menuName){
+		if(menuName == null || menuName.replaceAll(" ", "").equals("")){
+			return String.format(UPLOAD_IMAGE, "");
+		}
+		return String.format(UPLOAD_IMAGE, menuName + "/");
+	}
 
 }
